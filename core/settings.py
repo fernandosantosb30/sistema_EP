cbcbeeef2ef4603cd4305c969110ab9bdf0b20c3
fernandosantosb30 <fernandosantosb30@gmail.com>
2026-05-29 14:39:04 -0300

    Corrigindo leitura da SECRET_KEY

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get ('django-insecure-8&w-!j%a1xf8m_&7@6xc832x)^n1592kprw8lt48tcm&rsne5n')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

2f24cfa81367182a4a229eb3dd8ad389c5587d8f
fernandosantosb30 <fernandosantosb30@gmail.com>
2026-05-29 14:53:26 -0300

    Corrigindo localizacao do template de login

--- core/settings.py
+++ core/settings.py
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'sistema_isp', 'templates'), # Adicione esta linha
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

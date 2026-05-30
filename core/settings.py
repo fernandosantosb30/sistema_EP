8ec3338767c4b4860203fa2d8b3602488e0c47a1
fernandosantosb30 <fernandosantosb30@gmail.com>
2026-05-29 22:21:57 -0300

    Adicionando WhiteNoise para servir CSS

--- core/settings.py
+++ core/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # <--- ADICIONE ESTA LINHA AQUI
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

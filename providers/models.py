146a0a0fd2f54ac029fd32f91b315564aa52ea0f
fernandosantosb30 <fernandosantosb30@gmail.com>
2026-05-31 23:31:59 -0300

    contatos

--- providers/models.py
+++ providers/models.py
@@ -55,7 +55,7 @@ class Contato(models.Model):
     telefone = models.CharField(max_length=20, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
     # Adicione esta linha:
-    prioridade = models.IntegerField(default=1) 
+    prioridade = models.IntegerField(default=1, null=True, blank=True)
     
     class Meta:
         app_label = 'providers'

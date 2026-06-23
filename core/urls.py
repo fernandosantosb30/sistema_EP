c5da5580a97a5b508af5620059871ffb64bc74db
fernandosantosb30 <fernandosantosb30@gmail.com>
2026-06-23 09:33:36 -0300

    correcao log

--- core/urls.py
+++ core/urls.py
@@ -44,7 +44,6 @@ urlpatterns = [
     path('provedores/importar-cidades/<int:provedor_id>/', providers_views.importar_cidades_csv, name='importar_cidades_csv'),
     path('custo-medio/', providers_views.processar_custo_medio, name='processar_custo_medio'),
     path('custo-medio/processar-lote/', providers_views.processar_lote_csv, name='processar_lote'),
-    path('exportar-mapeamento/', views.exportar_provedores_view, name='exportar_mapeamento'),
     
     # Gestão de Usuários (Centralizado)
     path('usuarios/', providers_views.gestao_usuarios, name='gestao_usuarios'),
